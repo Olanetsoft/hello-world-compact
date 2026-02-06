ac021090af7b78fe49c3b980f184a62136a71041
Olanetsoft <heedris2olubisi@gmail.com>
2026-02-06 15:57:06 +0100

    Simplify contract structure and rename project

--- contract/src/index.ts
+++ contract/src/index.ts
@@ -1,2 +1,8 @@
-export * as Hello from "./managed/hello/contract/index.js";
-export * from "./witnesses";
+// Re-export the compiled contract as Hello namespace
+export * as Hello from './managed/hello/contract/index.js';
+
+// Private state type (empty for this contract)
+export type HelloPrivateState = {};
+
+// Witnesses (none needed for this contract)
+export const witnesses = {};
--- contract/src/witnesses.ts
+++
@@ -1,4 +0,0 @@
-// Hello World contract - no private state needed
-export type HelloPrivateState = {};
-
-export const witnesses = {};
